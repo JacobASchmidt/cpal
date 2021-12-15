1cf22c948ff0c180ecbcce688d0888e2396a4258
Jacob Schmidt <jacobalbertschmidt@gmail.com>
2021-12-15 16:27:49 -0600

    more fp

--- slices.go
+++ slices.go
@@ -78,3 +78,44 @@ func NoneOfZip[SliceA ~[]A, A any, SliceB ~[]B, B any](a SliceA, b SliceB, f fun
 	return true
 }
 
+func Reduce[Slice ~[]T, T any, R any](s Slice, init R, f func(R, T) R) R {
+	for _, val := range(s) {
+		init = f(init, val)
+	}
+	return init
+}
+
+func ZipReduce[SliceA ~[]A, A any, SliceB ~[]B, B any, R any](a SliceA, b SliceB, init R, f func(R, A, B) R) R {
+	n := Min(len(a), len(b))
+	for i := 0; i < n; i++ {
+		init = f(init, a[i], b[i])
+	}
+	return init
+}
+
+
+func MapReduce[Slice ~[]T, T any, R any](s Slice, init R, f func(R, T) R) R {
+	for _, val := range(s) {
+		init = f(init, val)
+	}
+	return init
+}
+
+func ZipMapReduce[SliceA ~[]A, A any, SliceB ~[]B, B any, R any, M any](a SliceA, b SliceB, map_ func(A, B) M, init R, reduce_ func(R, M) R) R {
+	n := Min(len(a), len(b))
+	for i := 0; i < n; i++ {
+		init = reduce_(init, map_(a[i], b[i]))
+	}
+	return init
+}
+
+
+func MapZipReduce[SliceA ~[]A, A any, SliceB ~[]B, B any, R any, AM any, BM any](a SliceA, b SliceB, mapA func(A) AM, mapB func(B) BM, init R, reduce_ func(R, AM, BM) R) R {
+	n := Min(len(a), len(b))
+	for i := 0; i < n; i++ {
+		init = reduce_(init, mapA(a[i]), mapB(b[i]))
+	}
+	return init
+}
+
+
